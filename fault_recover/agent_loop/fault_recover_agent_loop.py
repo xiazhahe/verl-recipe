8e543f14f02d2099fa854326d3269ce0b41c112c
Li-Yongwen <63399187+Li-Yongwen@users.noreply.github.com>
2026-01-23 13:03:38 +0800

    [feat] Fault detect and auto recover for rollout with backed up tokens (#14)
    ### What does this PR do?
    
    High available for vllm rollout. 
    [RFC](https://github.com/volcengine/verl/discussions/4355)
    
    ### Checklist Before Starting
    
    - [ ] Search for similar PRs. Paste at least one query link here: ...
    - [ ] Format the PR title as `[{modules}] {type}: {description}` (This
    will be checked by the CI)
    - `{modules}` include `fsdp`, `megatron`, `sglang`, `vllm`, `rollout`,
    `trainer`, `ci`, `training_utils`, `recipe`, `hardware`, `deployment`,
    `ray`, `worker`, `single_controller`, `misc`, `perf`, `model`, `algo`,
    `env`, `tool`, `ckpt`, `doc`, `data`, `cfg`, `reward`
    - If this PR involves multiple modules, separate them with `,` like
    `[megatron, fsdp, doc]`
      - `{type}` is in `feat`, `fix`, `refactor`, `chore`, `test`
    - If this PR breaks any API (CLI arguments, config, function signature,
    etc.), add `[BREAKING]` to the beginning of the title.
      - Example: `[BREAKING][fsdp, megatron] feat: dynamic batching`
    
    ### Solution
    
    <img width="1282" height="621" alt="0120"
    src="https://github.com/user-attachments/assets/58127f8f-c3f8-43f2-9e54-198cfd22d705"
    />
    
    ### API and Usage Example
    
    ```shell
    # code
    git clone https://github.com/verl-project/verl.git
    cd verl/recipe
    git pull origin main
    
    # run
    python3 -m recipe.fault_recover.main_ppo --config-path=config \
        --config-name='fault_recover_ppo_megatron_trainer.yaml'\
        fault_manager.enable = True \
        actor_rollout_ref.rollout.agent.default_agent_loop=fault_recover_single_turn_agent \
        # refer to other detal config in the fault_manager part of
        # recipe/fault_recover/config/fault_recover_ppo_megatron_trainer.yaml
    ```
    
    ### Design & Code Changes
    
    - Add fault_recover document.
    
    ### Checklist Before Submitting
    
    > [!IMPORTANT]
    > Please check all the following items before requesting a review,
    otherwise the reviewer might deprioritize this PR for review.
    
    - [ ] Read the [Contribute
    Guide](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md).
    - [ ] Apply [pre-commit
    checks](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md#code-linting-and-formatting):
    `pre-commit install && pre-commit run --all-files --show-diff-on-failure
    --color=always`
    - [ ] Add / Update [the
    documentation](https://github.com/volcengine/verl/tree/main/docs).
    - [ ] Add unit or end-to-end test(s) to [the CI
    workflow](https://github.com/volcengine/verl/tree/main/.github/workflows)
    to cover all the code. If not feasible, explain why: ...
    - [ ] Once your PR is ready for CI, send a message in [the `ci-request`
    channel](https://verl-project.slack.com/archives/C091TCESWB1) in [the
    `verl` Slack
    workspace](https://join.slack.com/t/verl-project/shared_invite/zt-3855yhg8g-CTkqXu~hKojPCmo7k_yXTQ).
    (If not accessible, please try [the Feishu group
    (飞书群)](https://applink.larkoffice.com/client/chat/chatter/add_by_link?link_token=772jd4f1-cd91-441e-a820-498c6614126a).)
    - [ ] If your PR is related to the `recipe` submodule, please also
    update the reference to the submodule commit via `git submodule update
    --remote` or `cd recipe && git pull origin main`.

---
+++ fault_recover/agent_loop/fault_recover_agent_loop.py
@@ -0,0 +1,129 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import os
+from typing import Any, Optional
+from uuid import uuid4
+
+import ray
+from omegaconf import DictConfig
+
+from verl.experimental.agent_loop.agent_loop import AgentLoopManager, AgentLoopWorker, AsyncLLMServerManager
+from verl.single_controller.ray.base import RayResourcePool, RayWorkerGroup
+from verl.utils.rollout_trace import rollout_trace_op
+from verl.workers.rollout.replica import TokenOutput
+
+logger = logging.getLogger(__file__)
+logger.setLevel(os.getenv("VERL_LOGGING_LEVEL", "WARN"))
+
+
+class FaultRecoverAsyncLLMServerManager(AsyncLLMServerManager):
+    """
+    A class to manage multiple OpenAI compatible LLM servers. This class provides
+    - Load balance: least requests load balancing
+    - Sticky session: send multi-turn chat completions to same server for automatic prefix caching
+    """
+
+    @rollout_trace_op
+    async def generate(
+        self,
+        request_id,
+        *,
+        prompt_ids: list[int],
+        sampling_params: dict[str, Any],
+        image_data: Optional[list[Any]] = None,
+        video_data: Optional[list[Any]] = None,
+        global_id: int = None,
+    ) -> TokenOutput:
+        """Generate tokens from prompt ids.
+
+        Args:
+            request_id (str): request id for sticky session.
+            prompt_ids (List[int]): List of prompt token ids.
+            sampling_params (Dict[str, Any]): Sampling parameters for the chat completion.
+            global_id: Global batch id of req.
+
+        Returns:
+            TokenOutput: token output
+        """
+        server = self._choose_server(request_id)
+        new_request_id = uuid4().hex
+        if global_id is not None:
+            from recipe.fault_recover.fault_manager import get_tokens_queue
+
+            tokens_queue = get_tokens_queue()
+
+            if tokens_queue is not None:
+                await tokens_queue.put.remote((new_request_id, global_id))
+
+        output = await server.generate.remote(
+            request_id=new_request_id,  # use new request_id for each turn
+            prompt_ids=prompt_ids,
+            sampling_params=sampling_params,
+            image_data=image_data,
+            video_data=video_data,
+        )
+        return output
+
+
+class FaultRecoverAgentLoopWorker(AgentLoopWorker):
+    """Agent loop worker takes a batch of messages and run each message in an agent loop."""
+
+    def __init__(
+        self,
+        config: DictConfig,
+        server_handles: list[ray.actor.ActorHandle],
+        reward_router_address: str = None,
+    ):
+        super().__init__(config, server_handles, reward_router_address)
+        self.server_manager = FaultRecoverAsyncLLMServerManager(config, server_handles)
+
+
+class FaultRecoverAgentLoopManager(AgentLoopManager):
+    """Agent loop manager that manages a group of agent loop workers."""
+
+    def __init__(
+        self, config: DictConfig, worker_group: RayWorkerGroup = None, rm_resource_pool: RayResourcePool = None
+    ):
+        """Initialize agent loop manager.
+
+        Args:
+            config (DictConfig): trainer config.
+            worker_group (RayWorkerGroup): ActorRolloutRef worker group for hybrid mode; None for standalone mode.
+            rm_resource_pool (RayResourcePool): Resource pool for reward model (Standalone mode).
+        """
+        self.config = config
+        self.worker_group = worker_group
+        self.reward_model_manager = None
+        self.reward_router_address = None
+        if self.config.reward_model.enable and self.config.reward_model.enable_resource_pool:
+            from verl.experimental.reward_loop import RewardModelManager
+
+            self.reward_model_manager = RewardModelManager(config.reward_model, rm_resource_pool)
+            self.reward_router_address = self.reward_model_manager.get_router_address()
+
+        # for recipe to change
+        if not hasattr(self, "rollout_replica_class"):
+            from recipe.fault_recover.vllm_rollout.vllm_async_server import FaultRecovervLLMReplica
+
+            self.rollout_replica_class = FaultRecovervLLMReplica
+        if not hasattr(self, "agent_loop_workers_class"):
+            self.agent_loop_workers_class = ray.remote(FaultRecoverAgentLoopWorker)
+
+        self._initialize_llm_servers()
+        self._init_agent_loop_workers()
+
+        # Initially we're in sleep mode.
+        if self.config.actor_rollout_ref.rollout.free_cache_engine:
+            self.sleep()
---
+++ fault_recover/vllm_rollout/vllm_async_server.py
@@ -0,0 +1,87 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import argparse
+import inspect
+import logging
+
+import ray
+import vllm
+from packaging import version
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.entrypoints.openai.api_server import (
+    build_app,
+    init_app_state,
+)
+from vllm.usage.usage_lib import UsageContext
+
+from verl.workers.config import HFModelConfig, RolloutConfig
+from verl.workers.rollout.utils import run_unvicorn
+from verl.workers.rollout.vllm_rollout.vllm_async_server import vLLMHttpServer, vLLMReplica
+
+_VLLM_VERSION = version.parse(vllm.__version__)
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+
+
+class FaultRecovervLLMHttpServer(vLLMHttpServer):
+    """vLLM http server in single node, this is equivalent to launch server with command line:
+    ```
+    vllm serve --tensor-parallel-size=8 ...
+    ```
+    """
+
+    async def run_server(self, args: argparse.Namespace):
+        from recipe.fault_recover.async_llm import AsyncFaultRecoverLLM as AsyncLLM
+
+        engine_args = AsyncEngineArgs.from_cli_args(args)
+        usage_context = UsageContext.OPENAI_API_SERVER
+        vllm_config = engine_args.create_engine_config(usage_context=usage_context)
+        vllm_config.parallel_config.data_parallel_master_port = self._dp_master_port
+
+        fn_args = set(dict(inspect.signature(AsyncLLM.from_vllm_config).parameters).keys())
+        kwargs = {}
+        if "enable_log_requests" in fn_args:
+            kwargs["enable_log_requests"] = engine_args.enable_log_requests
+        if "disable_log_stats" in fn_args:
+            kwargs["disable_log_stats"] = engine_args.disable_log_stats
+
+        engine_client = AsyncLLM.from_vllm_config(vllm_config=vllm_config, usage_context=usage_context, **kwargs)
+
+        # Don't keep the dummy data in memory
+        await engine_client.reset_mm_cache()
+
+        app = build_app(args)
+        if _VLLM_VERSION > version.parse("0.11.0"):
+            await init_app_state(engine_client, app.state, args)
+        else:
+            await init_app_state(engine_client, vllm_config, app.state, args)
+        if self.replica_rank == 0 and self.node_rank == 0:
+            logger.info(f"Initializing a V1 LLM engine with config: {vllm_config}")
+
+        self.engine = engine_client
+        self._server_port, self._server_task = await run_unvicorn(app, args, self._server_address)
+
+
+class FaultRecovervLLMReplica(vLLMReplica):
+    def __init__(
+        self,
+        replica_rank: int,
+        config: RolloutConfig,
+        model_config: HFModelConfig,
+        gpus_per_node: int = 8,
+        is_reward_model: bool = False,
+    ):
+        super().__init__(replica_rank, config, model_config, gpus_per_node, is_reward_model)
+        self.server_class = ray.remote(FaultRecovervLLMHttpServer)
